945fd7e4cf3b2baf059b0c8490e757c303a28d5f
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2024-06-14 12:15:07 +0700

    Update jabatanController.php

--- app/Http/Controllers/jabatanController.php
+++ app/Http/Controllers/jabatanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Jabatan;
 use Illuminate\Http\Request;
 
+class jabatanController extends Controller
 {
     /**
      * Display a listing of the resource.
