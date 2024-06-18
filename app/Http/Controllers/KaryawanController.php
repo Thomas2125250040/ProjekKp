3847e3a07f29a96812ad3d839528af9871e23948
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2024-06-18 21:03:27 +0700

    make: AbsensiController

---
+++ app/Http/Controllers/KaryawanController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Karyawan;
+use Illuminate\Http\Request;
+
+class KaryawanController extends Controller
+{
+    public function search() {
+        $filter = request()->query();
+        return Karyawan::where('nama_karyawan', 'like', "%{$filter['q']}%")->get();
+    }
+}
