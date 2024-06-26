5d29c976a48f5f680c124e77fdd740770b496cf7
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2024-06-26 08:56:24 +0700

    Create 2024_06_26_015141_create_gaji_table.php

---
+++ database/migrations/2024_06_26_015141_create_gaji_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gaji', function (Blueprint $table) {
+            $table->char('tahun', 4);
+            $table->char('bulan', 1);
+            $table->primary(['tahun', 'bulan']);
+            $table->string('id_jabatan');
+            $table->foreign('id_jabatan')->references('id')->on('jabatan')->cascadeOnUpdate();
+            $table->integer('gaji_pokok');
+            $table->mediumInteger('uang_makan');
+            $table->mediumInteger('uang_lembur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gaji');
+    }
+};
